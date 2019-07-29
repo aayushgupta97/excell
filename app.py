bde0c2cd5462684dc3abdaebbcb3d8c47f55729b
Aayush Gupta <aayushgupta2097@gmail.com>
2019-07-29 12:31:47 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from XL.columns import *
+
+# column_header_check = compare_column_csv('FS_OWR_20.csv', 'FS_OWR_20 (copy).csv')
+# column_header_check_2 = compare_column_excel('FS_OWR_20.csv', 'FS_OWR_20 (copy).csv')
+# column_header_check_3 = compare_column_csv_excel('FS_OWR_20.csv', 'FS_OWR_20.xlsx', 'Sheet1')
+# print(column_header_check_3)
